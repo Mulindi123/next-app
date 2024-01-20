49b1f887e7f5261515a37e582a4fcb1885a6448d
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-01-20 17:23:57 +0300

    daisy UI

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,22 +2,32 @@ import React from 'react'
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async() => {
  const res = await fetch(
-  'https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
+  'https://jsonplaceholder.typicode.com/users')
  const users: User[] = await res.json();
   return (
     <>
-      <h1 className='font-bold text-3xl text-blue-300'>Users</h1>
+      <h1 className='font-bold text-3xl'>Users</h1>
       <p className='text-2xl text-pink-800'>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map(user => 
-        <li key = {user.id}>
-          {user.name}
-        </li>)}
-      </ul>
+      <table className='table table-bordered '>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+        {users.map(user => <tr key = {user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+        </tr>)}
+        </tbody>
+        
+      </table>
     </>
   )
 }
